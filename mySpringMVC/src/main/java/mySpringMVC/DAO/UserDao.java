17af9a2a52af09d7190c2ef0abf19850e7f3d157
robertdenilo <b47849@hotmail.com>
2018-09-11 00:45:14 -0500

    0909

--- mySpringMVC/src/main/java/mySpringMVC/DAO/UserDao.java
+++
@@ -1,9 +0,0 @@
-package mySpringMVC.DAO;
-
-import mySpringMVC.ENTITY.User;
-
-public interface UserDao {
-
-	void addUser(User user) throws Exception;
-
-}
--- mySpringMVC/src/main/java/mySpringMVC/SERVICE/UserManage.java
+++
@@ -1,7 +0,0 @@
-package mySpringMVC.SERVICE;
-
-import mySpringMVC.ENTITY.User;
-
-public interface UserManage {
-     void addUser(User user) throws Exception;
-}
